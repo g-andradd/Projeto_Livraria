cb947a262495f59eea4fdfe224f516bf46d5d1a4
gabriel <gabrielnino123@outlook.com>
2022-02-17 21:49:50 -0300

    criando o Back

---
+++ back/vivilaria/src/main/java/br/com/vivilaria/model/Cliente.java
@@ -0,0 +1,66 @@
+package br.com.vivilaria.model;
+
+import java.util.Date;
+
+import javax.persistence.*;
+
+@Table(name = "clientes")
+@Entity
+public class Cliente {
+	@Id
+	private String cpf;
+	private String primeiroNome;
+	private String sobrenome;
+	private Date dataNascimento;
+	private String email;
+	private String senha;
+
+	public String getCpf() {
+		return cpf;
+	}
+
+	public void setCpf(String cpf) {
+		this.cpf = cpf;
+	}
+
+	public String getPrimeiroNome() {
+		return primeiroNome;
+	}
+
+	public void setPrimeiroNome(String primeiroNome) {
+		this.primeiroNome = primeiroNome;
+	}
+
+	public String getSobrenome() {
+		return sobrenome;
+	}
+
+	public void setSobrenome(String sobrenome) {
+		this.sobrenome = sobrenome;
+	}
+
+	public Date getDataNascimento() {
+		return dataNascimento;
+	}
+
+	public void setDataNascimento(Date dataNascimento) {
+		this.dataNascimento = dataNascimento;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getSenha() {
+		return senha;
+	}
+
+	public void setSenha(String senha) {
+		this.senha = senha;
+	}
+
+}
